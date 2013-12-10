c99542e58e9690caa696bf1823df5d4859b06310
icke2063 <icke@X230iSSD>
2013-12-10 11:13:48 +0100

    add renew delayed deadline function

--- include/ThreadPoolInt/DelayedPoolInt.h
+++ include/ThreadPoolInt/DelayedPoolInt.h
@@ -65,6 +65,12 @@ class DelayedFunctorInt{
    void resetDeadline(){gettimeofday(&m_deadline,0);}
 
    /**
+   * set Deadline to given deadline
+   */
+  void renewDeadline(struct timeval deadline){m_deadline = deadline;}
+
+
+   /**
     * get stored FunctorInt
     */
    shared_ptr<FunctorInt> getFunctor(){return m_functor;}
